fb3adf8ab3cb39ffde2e7e1e055eb3d1c0a7e4f1
ferdinandjason <ferdinandjasong@gmail.com>
2018-07-12 21:35:48 +0700

    make WelcomePage is Underconstruction

--- src/pages/cygnus/welcome/WelcomePage/WelcomePage.js
+++ src/pages/cygnus/welcome/WelcomePage/WelcomePage.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Icon } from '@blueprintjs/core';
+import { SinglePageLayout } from "../../../../components";
 
-export const WelcomePage = () => (
-    <div></div>
-);
+import style from './WelcomePage.scss';
+
+export class WelcomePage extends React.Component{
+    render(){
+        return (
+            <SinglePageLayout>
+                <h3 className={style.welcome_icon}>
+                    <Icon icon="oil-field" className="home__icon" iconSize={Icon.SIZE_LARGE} />
+                </h3>
+                <h3 className={style.welcome_text}>Dashboard under construction.</h3>
+            </SinglePageLayout>
+        )
+    }
+}
