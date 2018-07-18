2a6e73fbce2c0ba0b8f59a9452ebe7ad47f479f6
Unknown <yolandahertita903@gmail.com>
2018-07-18 18:33:39 +0700

    Auto stash before merge of "master" and "origin/master"
    welcome page

---
+++ src/pages/cygnus/welcome/WelcomePage/Submissions/SubmissionTable/LoadingSubmissionTable.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { Button } from '@blueprintjs/core';
+
+import style from './SubmissionTable.scss';
+
+class LoadingSubmissionTable extends React.Component {
+    render(){
+        return (
+            <table className={classNames("bp3-html-table","bp3-html-table-striped","bp3-interactive",style.submission_list_wrapper)}>
+                <tbody>
+                    <tr>
+                        <td>
+                            <p className={"bp3-skeleton"}>Placeholder for Username</p>
+                            <p className={"bp3-skeleton"}>Time</p>
+                        </td>
+                        <td><p className={"bp3-skeleton"}>Placeholder for Title</p></td>
+                        <td><p className={"bp3-skeleton"}>Status</p></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p className={"bp3-skeleton"}>Placeholder for Username</p>
+                            <p className={"bp3-skeleton"}>Time</p>
+                        </td>
+                        <td><p className={"bp3-skeleton"}>Placeholder for Title</p></td>
+                        <td><p className={"bp3-skeleton"}>Status</p></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p className={"bp3-skeleton"}>Placeholder for Username</p>
+                            <p className={"bp3-skeleton"}>Time</p>
+                        </td>
+                        <td><p className={"bp3-skeleton"}>Placeholder for Title</p></td>
+                        <td><p className={"bp3-skeleton"}>Status</p></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p className={"bp3-skeleton"}>Placeholder for Username</p>
+                            <p className={"bp3-skeleton"}>Time</p>
+                        </td>
+                        <td><p className={"bp3-skeleton"}>Placeholder for Title</p></td>
+                        <td><p className={"bp3-skeleton"}>Status</p></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p className={"bp3-skeleton"}>Placeholder for Username</p>
+                            <p className={"bp3-skeleton"}>Time</p>
+                        </td>
+                        <td><p className={"bp3-skeleton"}>Placeholder for Title</p></td>
+                        <td><p className={"bp3-skeleton"}>Status</p></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default LoadingSubmissionTable;
--- src/pages/cygnus/welcome/WelcomePage/WelcomePage.js
+++ src/pages/cygnus/welcome/WelcomePage/WelcomePage.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Icon } from '@blueprintjs/core';
 import { SinglePageLayout } from "../../../../components";
 
+import WelcomeHead from "./WelcomeHead/WelcomeHead";
+import NewProblemList from "./NewProblem/NewProblemList/NewProblemList";
+import SubmissionList from "./Submissions/SubmissionList/SubmissionList";
+
 import style from './WelcomePage.scss';
 
 export class WelcomePage extends React.Component{
     render(){
         return (
             <SinglePageLayout>
-                <h3 className={style.welcome_icon}>
-                    <Icon icon="oil-field" className="home__icon" iconSize={Icon.SIZE_LARGE} />
-                </h3>
-                <h3 className={style.welcome_text}>Dashboard under construction.</h3>
+                <div className={style.wh_background}>
+                    <div className={style.w_container_1}>
+                        <WelcomeHead/>
+                    </div>
+                </div>
+
+                <div className={style.w_container_2}>
+                    <NewProblemList/>
+                    <SubmissionList/>
+                </div>
+                
             </SinglePageLayout>
         )
     }
